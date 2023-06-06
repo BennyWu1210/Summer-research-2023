8effe78d5a6c75d986f6870ef9bc58fc375520c8
bennywu2019 <benny.wu.new@gmail.com>
2023-06-06 15:57:57 -0400

    added classification function

--- BIBD/generator.c
+++ BIBD/generator.c
@@ -8,6 +8,77 @@
 #include <stdlib.h>
 #include <assert.h>
 
+// for demonstration purposes - classify all codewords into files
+void classify(struct Matrix *generator)
+{
+    int basis_rows = get_rows(generator);
+    int basis_cols = get_cols(generator);
+    int mask_max = (1 << basis_rows);
+
+    int num_files = basis_cols;
+    FILE *classes[num_files];
+
+    char filename[34]; // assume that the file name does not exceed 34 characters
+
+    for (int i = 0; i < num_files; i++)
+    {
+        snprintf(filename, sizeof(filename), "./tests/classification/file%d.txt", i + 1);
+        classes[i] = fopen(filename, "w");
+
+        if (classes[i] == NULL)
+        {
+            printf("WARNING: FAILED TO OPEN FILE: %s\n", filename);
+        }
+        else
+        {
+            printf("File opened successfully: %s\n", filename);
+        }
+    }
+
+    // generate all codewords
+    for (int mask = 0; mask < mask_max; mask++)
+    {
+        struct Matrix *codeword = matrix_init(1, basis_cols);
+        for (int i = 0; i < basis_rows; i++)
+        {
+            if (mask & (1 << i))
+            {
+
+                for (int j = 0; j < basis_cols; j++)
+                {
+                    matrix_add(codeword, 0, j, get_value(generator, i, j));
+                }
+            }
+        }
+
+        // store the binary string of this codeword
+        char binary_val[basis_cols + 1];
+
+        for (int i = 0; i < basis_cols; i++)
+        {
+            binary_val[i] = get_value(codeword, 0, i) == 1 ? '1' : '0';
+        }
+
+        binary_val[basis_cols] = '\0';
+
+        // if the i-th bit is a 1, we store it in the i-th file
+        for (int i = 0; i < basis_cols; i++)
+        {
+            if (get_value(codeword, 0, i) == 1)
+            {
+                fprintf(classes[i], "%s\n", binary_val);
+            }
+        }
+        free(codeword);
+    }
+
+    // close all file pointers
+    for (int i = 0; i < num_files; i++)
+    {
+        fclose(classes[i]);
+    }
+}
+
 int main()
 {
     // the number of points, size of each block, and occurrences of every pair respectively
@@ -68,11 +139,13 @@ int main()
     struct Matrix *basis = get_row_basis(matrix);
 
     matrix_print(basis);
+
     // find all possible codewords by taking its linear combinations
     int basis_rows = get_rows(basis);
     int basis_cols = get_cols(basis);
     int mask_max = (1 << basis_rows);
 
+    // for testing purposes
     for (int mask = 0; mask < mask_max; mask++)
     {
         struct Matrix *codeword = matrix_init(1, b);
@@ -90,6 +163,8 @@ int main()
 
         free(codeword);
     }
+
+    classify(basis);
 
     // release memory
     free(original_matrix);
